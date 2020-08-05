2e3fb9314a870c2851be36e89f24075d11fb0020
Sruthi JT <sruthi91dec2@gmail.com>
2020-08-05 17:53:58 +0530

    added profile picture, story, bootstrap, favicon, footer, colors etc.

---
+++ js/myProfile.js
@@ -0,0 +1,35 @@
+function openNav() {
+	document.getElementById("mySidenav").style.width = "250px";
+}
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
+$(window).scroll(function(){				 
+	$('.story p').each(function(){
+		var scrollTop = $(window).scrollTop(),
+		elementOffset = $(this).offset().top,
+		distance      = (elementOffset - scrollTop),
+		windowHeight  = $(window).height(),
+		breakPoint    = windowHeight*0.9;
+
+		if(distance > breakPoint) {
+			$(this).addClass("more-padding");	
+		}
+		if(distance < breakPoint) {
+			$(this).removeClass("more-padding");	
+		}
+	});
+});
+$(document).ready(function() {
+	$("#core-skills").mouseenter(function() {
+		setInterval(function() {
+			window.location.reload();
+			}, 500);
+	});
+	$("#adv-skills").mouseenter(function() {
+		setInterval(function() {
+			window.location.reload();
+			}, 500);
+	});
+});
